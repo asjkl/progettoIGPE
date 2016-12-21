5bef961a697de7de2fc6b0b03f43b9367465d7a6
Giovanni <Giovanni@Giovanni.homenet.telecomitalia.it>
2016-12-21 17:41:09 +0100

    PROVARE IL DOPPIO ROCKET SPARANDO AL MURO..RISPETTO AL METODO DI DAVIDE, HO UTILIZZATO UNA BOOLEANA CHE HA OGNI ROCKET, MA CHE MI SERVE SOLO PER I ROCKET DEL PLAYER...

--- Rocket.java
+++ Rocket.java
@@ -8,6 +8,9 @@ public class Rocket extends AbstractDynamicObject {
 	
 	//PER GRAFICA
 	private boolean firstAnimationNo;
+	private boolean rocketForPlayer;		//ANCHE SE IMPOSTATO A TUTTI I ROCKET, MI SERVE SOLO PER I ROCKET DEL PLAYER....
+											//SE è TRUE NON LO FACCIO AGGIORNARE NE LOGICAMENTE E NE GRAFICAMENTE, INFATTI QUESTA BOOL
+											//MI SERVE PER FORZARE IL BLOCCO GRAFICO DEL SECONDO ROCKET
 	
 	public Rocket(int x, int y, World world, Direction direction, AbstractDynamicObject tank) {
 		super(x, y, world, direction);
@@ -17,13 +20,27 @@ public class Rocket extends AbstractDynamicObject {
 		this.setBeforeBordo(tank.getCurr());
 		
 		this.firstAnimationNo=true;
-		setUpdateObject(true);
+		
 		if(tank.getSpeedShot()==Speed.SLOW){
 			this.setCont(1);
 		}else if(tank.getSpeedShot()==Speed.NORMAL){
 			this.setCont(getSizePixel()/2);
 		}else{			
 			this.setCont((getSizePixel()/2)+5);
+		}
+		
+		if(tank instanceof PlayerTank){
+			if(tank.getContRocket()<1){	
+				setUpdateObject(true);
+				rocketForPlayer=true;
+			}
+			else{
+				setUpdateObject(false);
+				rocketForPlayer=false;
+			}
+		}else{
+			setUpdateObject(true);
+			rocketForPlayer=true;
 		}
 	}
 
@@ -83,5 +100,13 @@ public class Rocket extends AbstractDynamicObject {
 
 	public void setFirstAnimationNo(boolean firstAnimationNo) {
 		this.firstAnimationNo = firstAnimationNo;
+	}
+
+	public boolean isRocketForPlayer() {
+		return rocketForPlayer;
+	}
+
+	public void setRocketForPlayer(boolean rocketForPlayer) {
+		this.rocketForPlayer = rocketForPlayer;
 	}
 }
