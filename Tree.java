eca172c4577aa16b04bdfaf9f493638593c03843
Aloia <Aloia@Aloia-PC>
2017-04-26 15:46:02 +0200

    PULITO!!

---
+++ Tree.java
@@ -0,0 +1,13 @@
+package progettoIGPE.davide.giovanni.unical2016;
+
+public class Tree extends AllWall {
+
+	@Override
+	public String toString() {
+		return " TT ";
+	}          
+
+	public Tree(int x, int y, World world) {
+		super(x, y, world, true, true);
+	}
+}
